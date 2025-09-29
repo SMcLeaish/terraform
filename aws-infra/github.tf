fbb1d1bf8c28f0045ae92a765ef0645581b044d9
Sean McLeaish <smcleaish@gmail.com>
2025-09-29 14:31:53 -0400

    Updated github action for test

--- aws-infra/github.tf
+++ aws-infra/github.tf
@@ -104,3 +104,8 @@ output "github_actions_role_arn" {
   value = aws_iam_role.github_actions.arn
 }
 
+output "github_actions_terraform_role_arn"{
+  value = aws_iam_role.github_actions_terraform.arn
+}
+
+
